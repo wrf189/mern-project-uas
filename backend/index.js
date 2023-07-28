167927c2cc9a4f040e180c9acca631e0ef1a3d56
wrf189 <74730980+wrf189@users.noreply.github.com>
2023-07-29 01:31:47 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,13 +11,6 @@ import bookingRoute from './routes/bookings.js';
 
 dotenv.config();
 const app = express();
-app.use(cors(
-  {
-    origin: ["https://project-uas-mern.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true
-  }
-));
 
 mongoose.set("strictQuery", false);
 
@@ -43,7 +36,10 @@ app.use("/api/v1/users", userRoute);
 app.use("/api/v1/review", reviewRoute);
 app.use("/api/v1/booking", bookingRoute);
 
-app.listen(port, () => {
-  connect();
-  console.log('server listening on port', port);
-});
+app.use(cors(
+  {
+    origin: ["https://project-uas-mern.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
