0ed07b482e1678010af5e9d8910f529a4d0c4e18
wrf189 <74730980+wrf189@users.noreply.github.com>
2023-07-30 00:31:20 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 8000
 const corsOptions = {
-   origin: true,
+   origin: "https://merntravel-uas.onrender.com",
    credentials: true
 }
 
@@ -34,11 +34,11 @@ const connect = async() => {
 app.use(express.json())
 app.use(cors(corsOptions))
 app.use(cookieParser())
-app.use("/api/v1/auth", authRoute)
-app.use("/api/v1/tours", tourRoute)
-app.use("/api/v1/users", userRoute)
-app.use("/api/v1/review", reviewRoute)
-app.use("/api/v1/booking", bookingRoute)
+app.use("/auth", authRoute)
+app.use("/tours", tourRoute)
+app.use("/users", userRoute)
+app.use("/review", reviewRoute)
+app.use("/booking", bookingRoute)
 
 app.listen(port, () => {
    connect()
